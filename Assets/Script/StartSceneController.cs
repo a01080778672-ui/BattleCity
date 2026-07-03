8c6709297c4c3dafe3215d9383bf6f05a27f9132
KIMIAN0826 <iandana0612@gmail.com>
2026-07-03 21:07:13 +0900

    start scene
    시작화면 만들었습니다

---
+++ Assets/Script/StartSceneController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartSceneController : MonoBehaviour
+{
+    [SerializeField] private string gameSceneName = "GameScene";
+
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene(gameSceneName);
+    }
+}
